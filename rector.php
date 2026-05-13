e2ddc157078205f3b1ab53f1b447b9af9eb9705f
Emaad Ali <emaadali@users.noreply.github.com>
2026-05-12 21:51:26 -0400

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,25 +2,74 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPublicMethodParameterRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Rector\ClassMethod\MakeModelAttributesAndScopesProtectedRector;
+use RectorLaravel\Rector\If_\AbortIfRector;
+use RectorLaravel\Rector\If_\ReportIfRector;
+use RectorLaravel\Rector\If_\ThrowIfRector;
+use RectorLaravel\Rector\MethodCall\RedirectRouteToToRouteHelperRector;
+use RectorLaravel\Rector\StaticCall\EloquentMagicMethodToQueryBuilderRector;
+use RectorLaravel\Rector\StaticCall\MinutesToSecondsInCacheRector;
+use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withSets([
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        LaravelSetList::LARAVEL_FACTORIES,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+    ])
+    ->withImportNames()
+    ->withRules([
+        MinutesToSecondsInCacheRector::class,
+    ])
+    ->withComposerBased(laravel: true)
+    ->withCache(
+        cacheDirectory: '/tmp/rector',
+        cacheClass: FileCacheStorage::class,
+    )
     ->withPaths([
         __DIR__.'/app',
         __DIR__.'/bootstrap/app.php',
+        __DIR__.'/config',
         __DIR__.'/database',
         __DIR__.'/public',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     ->withSkip([
+        __DIR__.'/app/Support/IdeHelper',
+        MakeModelAttributesAndScopesProtectedRector::class,
+        EncapsedStringsToSprintfRector::class,
+        ClosureToArrowFunctionRector::class,
+        EloquentMagicMethodToQueryBuilderRector::class,
+        RedirectRouteToToRouteHelperRector::class,
+        ThrowIfRector::class,
+        AbortIfRector::class,
+        ReportIfRector::class,
         AddOverrideAttributeToOverriddenMethodsRector::class,
+        RemoveUnusedPublicMethodParameterRector::class,
     ])
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
         codingStyle: true,
+        //        privatization: true,
+        typeDeclarations: true,
+        earlyReturn: true,
     )
     ->withPhpSets();
